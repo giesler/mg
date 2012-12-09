7059c827b7e090878faae8a296f4f04061cfbf33
mike <mike@sp.tfs.local>
2012-12-09 23:33:09 +0000

    {8/5/2001 4:14:18 AM}AMSFeatured.ascx.cs (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 3428;CreationDate: 2011-01-02 07:14:16 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C7568

---
+++ AMS/Web Sites/AMS Welcome/AMSFeatured.ascx.cs
@@ -0,0 +1,46 @@
+namespace AMS_Welcome
+{
+	using System;
+	using System.Data;
+	using System.Drawing;
+	using System.Web;
+	using System.Web.UI.WebControls;
+	using System.Web.UI.HtmlControls;
+
+	/// <summary>
+	///		Summary description for AMSFeatured.
+	/// </summary>
+	public abstract class AMSFeatured : System.Web.UI.UserControl
+	{
+
+
+		/// <summary>
+		public AMSFeatured()
+		{
+			this.Init += new System.EventHandler(Page_Init);
+		}
+
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			// Put user code to initialize the page here
+		}
+
+		private void Page_Init(object sender, EventArgs e)
+		{
+			//
+			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
+			//
+			InitializeComponent();
+		}
+
+		#region Web Form Designer generated code
+		///		Required method for Designer support - do not modify
+		///		the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Load += new System.EventHandler(this.Page_Load);
+		}
+		#endregion
+	}
+}
