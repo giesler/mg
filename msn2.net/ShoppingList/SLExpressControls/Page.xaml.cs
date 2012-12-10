2cf236d168544d539a1a548efe8c51489db5f363
mike <mike@sp.tfs.local>
2012-12-10 00:30:51 +0000

    {3/9/2008 6:44:08 PM~3/9/2008 6:44:10 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 4259;CreationDate: 2011-01-02 07:44:03 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8367

--- msn2.net/ShoppingList/SLExpressControls/Page.xaml.cs
+++ msn2.net/ShoppingList/SLExpressControls/Page.xaml.cs
@@ -16,6 +16,7 @@ namespace SLExpressControls
     {
         ShoppingListServiceClient client = new ShoppingListServiceClient();
         List<string> stores = null;
+        bool loadingItems = false;
 
         public Page()
         {
@@ -27,8 +28,12 @@ namespace SLExpressControls
 
         void client_GetStoresCompleted(object sender, GetStoresCompletedEventArgs e)
         {
+            client.GetStoresCompleted -= new EventHandler<GetStoresCompletedEventArgs>(client_GetStoresCompleted);
+
+            var q = from i in e.Result select new StoreListItem { Store = i };
+
             this.stores = e.Result;
-            this.storeList.ItemsSource = e.Result;
+            this.storeList.ItemsSource = q;
             this.storeList.SelectedItem = this.storeList.Items[0];
 
             this.LoadItems();
@@ -36,23 +41,31 @@ namespace SLExpressControls
 
         private void storeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //this.LoadItems();
+            this.LoadItems();
         }
 
         void LoadItems()
         {
-            StoreListItem item = (StoreListItem) this.storeList.SelectedItem;
-            this.Log("Loading {0}", item.Store);
+            if (this.storeList.SelectedIndex >= 0)
+            {
+                if (this.loadingItems == false)
+                {
+                    StoreListItem item = (StoreListItem)this.storeList.SelectedItem;
+                    this.Log("Loading {0}", item.Store);
 
-            client.GetShoppingListItemsCompleted += new EventHandler<GetShoppingListItemsCompletedEventArgs>(client_GetShoppingListItemsCompleted);
-            client.GetShoppingListItemsAsync(item.Store);
+                    this.client.GetShoppingListItemsCompleted += new EventHandler<GetShoppingListItemsCompletedEventArgs>(client_GetShoppingListItemsCompleted);
+                    this.client.GetShoppingListItemsAsync(item.Store);
 
-            this.items.Visibility = Visibility.Collapsed;
-            this.loading.Visibility = Visibility.Visible;
+                    this.items.Visibility = Visibility.Collapsed;
+                    this.loading.Visibility = Visibility.Visible;
+                }
+            }
         }
 
         void client_GetShoppingListItemsCompleted(object sender, GetShoppingListItemsCompletedEventArgs e)
         {
+            this.client.GetShoppingListItemsCompleted -= new EventHandler<GetShoppingListItemsCompletedEventArgs>(client_GetShoppingListItemsCompleted);
+
             StoreListItem selectedStore = (StoreListItem)this.storeList.SelectedItem;
             string loadedStore = e.UserState.ToString();
 
@@ -60,23 +73,29 @@ namespace SLExpressControls
 
             if (loadedStore.Equals(selectedStore.Store, StringComparison.InvariantCultureIgnoreCase))
             {
+                this.loadingItems = true;
+
                 this.items.ItemsSource = e.Result.Where(r => r.Store.Equals(selectedStore.Store, StringComparison.InvariantCultureIgnoreCase));
 
                 int selectedIndex = this.storeList.SelectedIndex;
-                this.storeList.Items.Clear();
+
+                List<StoreListItem> itemList = new List<StoreListItem>();
                 foreach (string store in this.stores)
                 {
                     var q = from i in e.Result
                             where i.Store.Equals(store, StringComparison.InvariantCultureIgnoreCase)
                             select i;
 
-                    StoreListItem item = new StoreListItem { Store = store, ItemCount = q.Count() };
-                    this.storeList.Items.Add(item);
+                    itemList.Add(new StoreListItem { Store = store, ItemCount = q.Count() });
                 }
+                
+                this.storeList.ItemsSource = itemList;
                 this.storeList.SelectedIndex = selectedIndex;
 
                 this.items.Visibility = Visibility.Visible;
                 this.loading.Visibility = Visibility.Collapsed;
+
+                this.loadingItems = false;
             }
         }
 
@@ -88,6 +107,8 @@ namespace SLExpressControls
 
         void client_AddShoppingListItemCompleted(object sender, AddShoppingListItemCompletedEventArgs e)
         {
+            client.AddShoppingListItemCompleted -= new EventHandler<AddShoppingListItemCompletedEventArgs>(client_AddShoppingListItemCompleted);
+
             if (e.Cancelled == false)
             {
                 if (e.UserState.ToString() == this.storeList.SelectedItem.ToString())
@@ -110,8 +131,18 @@ namespace SLExpressControls
         public void Log(string format, params object[] args)
         {
             string message = string.Format(format, args);
-            this.debug.Text += message + Environment.NewLine;
+            this.debug.Text += string.Format("{0:00}:{1:00}:{2:00}: {3}{4}", 
+                DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second,
+                message, Environment.NewLine);
             this.debug.SelectionStart = this.debug.Text.Length;
+        }
+
+        private void edit_Click(object sender, RoutedEventArgs e)
+        {
+            HyperlinkButton link = (HyperlinkButton)sender;
+            this.editor.DataContext = link.DataContext;
+
+            this.editor.Visibility = Visibility.Visible;
         }
     }
 
@@ -128,7 +159,7 @@ namespace SLExpressControls
             }
             else
             {
-                return base.ToString();
+                return this.Store;
             }
         }
     }
