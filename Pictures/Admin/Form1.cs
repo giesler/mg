777275b13f272c4fa9730de97257e932191b04e4
mike <mike@sp.tfs.local>
2012-12-10 00:24:28 +0000

    {8/18/2006 6:04:48 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 4133;CreationDate: 2011-01-02 07:39:16 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8241

--- Pictures/Admin/Form1.cs
+++ Pictures/Admin/Form1.cs
@@ -131,6 +131,7 @@ namespace msn2.net.Pictures
             try
             {
                 msn2.net.Pictures.Controls.fMain f = new msn2.net.Pictures.Controls.fMain();
+
                 Application.Run(f);
             }
             catch (Exception ex)
