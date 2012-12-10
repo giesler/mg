757e4c861f6c46339f8419d208016da6f3a03a97
mike <mike@sp.tfs.local>
2012-12-10 00:24:46 +0000

    {6/24/2007 3:09:56 PM~6/24/2007 3:10:30 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 4140;CreationDate: 2011-01-02 07:39:21 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8248

---
+++ msn2.net/BarMonkey/RelayControllerTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RelayControllerTest.ServiceReference;
+
+namespace RelayControllerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RelayControllerClient client = new RelayControllerClient();
+
+            BatchItem[] batch = new BatchItem[] {  
+                                new BatchItem { Group = 1, RelayNumber = 3, Seconds = 2},
+                                new BatchItem { Group = 1, RelayNumber = 4, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 5, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 8, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 10, Seconds = 4},
+                                new BatchItem { Group = 2, RelayNumber = 12, Seconds = 5},
+                                new BatchItem { Group = 2, RelayNumber = 14, Seconds = 8},
+                                new BatchItem { Group = 2, RelayNumber = 16, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 18, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 20, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 22, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 24, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 26, Seconds = 5},
+                                new BatchItem { Group = 2, RelayNumber = 28, Seconds = 4},
+                                new BatchItem { Group = 2, RelayNumber = 30, Seconds = 8},
+                                new BatchItem { Group = 2, RelayNumber = 17, Seconds = 2},
+                                new BatchItem { Group = 2, RelayNumber = 15, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 13, Seconds = 4},
+                                new BatchItem { Group = 2, RelayNumber = 25, Seconds = 8},
+                                new BatchItem { Group = 2, RelayNumber = 27, Seconds = 4},
+                                new BatchItem { Group = 2, RelayNumber = 29, Seconds = 3},
+                                new BatchItem { Group = 2, RelayNumber = 9, Seconds = 6},
+                                new BatchItem { Group = 2, RelayNumber = 11, Seconds = 4},
+                                new BatchItem { Group = 2, RelayNumber = 31, Seconds = 7}
+                                };
+
+            client.SendBatch(batch);
+
+            Console.Read();
+        }
+
+
+    }
+}
