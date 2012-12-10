6665398abb3f7357ba01e8e44bcd7902944f8759
mike <mike@sp.tfs.local>
2012-12-10 00:24:38 +0000

    {4/16/2007 10:40:16 PM~4/16/2007 10:42:26 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 4137;CreationDate: 2011-01-02 07:39:18 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8245

--- Pictures/Api/Utilities.cs
+++ Pictures/Api/Utilities.cs
@@ -23,7 +23,7 @@ namespace msn2.net.Pictures
         public static PictureConfig Load()
 		{
 			PictureConfig config;
-            config.ConnectionString = "data source=stan.sp.msn2.net;initial catalog=picdb;User ID=PictureAdminUser;Password=si82izmwiae2;persist security info=False";
+            config.ConnectionString = "data source=stan;initial catalog=picdb;User ID=PictureAdminUser;Password=si82izmwiae2;persist security info=False";
             config.PictureDirectory = @"\\stan.sp.msn2.net\Pictures\pics.msn2.net\";
             config.SmtpServer		= "192.168.1.5";
             config.CacheDirectory = @"\\ned.sp.msn2.net\picCache\";
