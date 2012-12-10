e0cf95efa51f3e8f893a3d2a5cff4457898da7e8
mike <mike@sp.tfs.local>
2012-12-10 00:24:53 +0000

    {6/24/2007 3:23:04 PM~6/24/2007 3:23:18 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 4142;CreationDate: 2011-01-02 07:39:23 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8250

---
+++ msn2.net/BarMonkey/RelayControllerLibrary/ConsoleRelayController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace msn2.net.BarMonkey.RelayController
+{
+    public class ConsoleRelayController: IRelayController
+    {
+        #region IRelayController Members
+
+        public void ConnectTest()
+        {
+            Console.WriteLine("ConnectTest called.");
+        }
+
+        public void SendBatch(List<BatchItem> batch)
+        {
+            Console.WriteLine("SendBatch - {0} items", batch.Count);
+            foreach (BatchItem item in batch)
+            {
+                Console.WriteLine(" - Group {0}, Relay {1} for {2} seconds", item.Group, item.RelayNumber, item.Seconds);
+            }
+        }
+
+        #endregion
+    }
+}
