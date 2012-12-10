5fb5d7ef73e932ab31f337174f47df40314dd615
mike <mike@sp.tfs.local>
2012-12-10 02:45:04 +0000

    {12/19/2001 6:53:08 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 3778;CreationDate: 2011-01-02 07:21:22 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8517

---
+++ Mike/VBSW/v3/App/Button.h
@@ -0,0 +1,20 @@
+// Button.h: interface for the CButton class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_BUTTON_H__BF3C81F1_8E99_4395_909E_29D1C2D5DCB0__INCLUDED_)
+#define AFX_BUTTON_H__BF3C81F1_8E99_4395_909E_29D1C2D5DCB0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CButton  
+{
+public:
+	CButton();
+	virtual ~CButton();
+
+};
+
+#endif // !defined(AFX_BUTTON_H__BF3C81F1_8E99_4395_909E_29D1C2D5DCB0__INCLUDED_)
