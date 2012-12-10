7b418f01587e9fa98743ee6e4e51e326e10cf3e5
mike <mike@sp.tfs.local>
2012-12-10 02:45:27 +0000

    {1/9/2002 5:00:50 PM} (TFS Integration from 'vsts.sp.msn2.net (VC)' Id: 3788;CreationDate: 2011-01-02 07:32:08 UTC;)
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8524

---
+++ Mike/VBSW/v3/App/CommandLineInfoEx.cpp
@@ -0,0 +1,27 @@
+// CommandLineInfoEx.cpp: implementation of the CCommandLineInfoEx class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "autorun.h"
+#include "CommandLineInfoEx.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CCommandLineInfoEx::CCommandLineInfoEx()
+{
+
+}
+
+CCommandLineInfoEx::~CCommandLineInfoEx()
+{
+
+}
---
+++ Mike/VBSW/v3/App/CommandLineInfoEx.h
@@ -0,0 +1,20 @@
+// CommandLineInfoEx.h: interface for the CCommandLineInfoEx class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_COMMANDLINEINFOEX_H__EEF4D517_976F_4A69_9195_51F0AE62B92F__INCLUDED_)
+#define AFX_COMMANDLINEINFOEX_H__EEF4D517_976F_4A69_9195_51F0AE62B92F__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CCommandLineInfoEx : public CCommandLineInfo  
+{
+public:
+	CCommandLineInfoEx();
+	virtual ~CCommandLineInfoEx();
+
+};
+
+#endif // !defined(AFX_COMMANDLINEINFOEX_H__EEF4D517_976F_4A69_9195_51F0AE62B92F__INCLUDED_)
