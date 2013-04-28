dd2c8cb7ada339fbb2c7061c6908095b5374da58
Mike Giesler <giesler@live.com>
2013-04-28 22:41:54 +0000

    Cam app video UI updates
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8565

--- msn2.net/chickweb/ChickPhone/Video.xaml.cs
+++ msn2.net/chickweb/ChickPhone/Video.xaml.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.Phone.Controls;
 using System;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace ChickPhone
 {
     public partial class Video : PhoneApplicationPage
     {
+        DispatcherTimer timer = new DispatcherTimer();
+
         public Video()
         {
             InitializeComponent();
+
+            this.timer.Interval = TimeSpan.FromMilliseconds(250);
+            this.timer.Tick += timer_Tick;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            this.progress.Value = this.player.Position.TotalMilliseconds;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -18,6 +29,19 @@ namespace ChickPhone
             string video = NavigationContext.QueryString["v"];
 
             this.player.Source = new Uri("http://cams.msn2.net/GetVid.aspx?v=" + video);
+        }
+
+        private void player_MediaOpened(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.pb.Visibility = System.Windows.Visibility.Collapsed;
+            this.progress.Visibility = System.Windows.Visibility.Visible;
+            this.progress.Maximum = this.player.NaturalDuration.TimeSpan.TotalMilliseconds;
+            this.timer.Start();
+        }
+
+        private void player_MediaEnded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
         }
     }
 }
