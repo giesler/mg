4ad004e489c37710837cbdd066442d01ce7582cc
Mike Giesler <giesler@live.com>
2014-02-09 19:51:22 +0000

    Config updates for Azure hosting
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8632

--- Pictures/Api/Utilities.cs
+++ Pictures/Api/Utilities.cs
@@ -23,10 +23,10 @@ namespace msn2.net.Pictures
         public static PictureConfig Load()
 		{
 			PictureConfig config;
-            config.ConnectionString = "data source=kenny;initial catalog=picdb;User ID=PictureAdminUser;Password=si82izmwiae2;persist security info=False";
-            config.PictureDirectory = @"\\barbrady\Pictures\pics.msn2.net\";
+            config.ConnectionString = "data source=.;initial catalog=picdb;User ID=picuser;Password=89203adlkE;persist security info=False";
+            config.PictureDirectory = @"c:\pics.msn2.net\";
             config.SmtpServer		= "mail.sp.msn2.net";
-            config.CacheDirectory = @"\\barbrady\picCache\";
+            config.CacheDirectory = @"c:\inetpub\pics.msn2.net\picCache\";
             return config;
 		}
 	}
