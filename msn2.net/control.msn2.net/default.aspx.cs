16821ea89e6966fecd6bec16cacedf3c531ee5a6
Mike Giesler <giesler@live.com>
2015-06-21 01:49:06 +0000

    Add control.msn2.net
    git-tfs-id: [https://giesler.visualstudio.com/]$/main;C8667

---
+++ msn2.net/control.msn2.net/default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class control : System.Web.UI.Page
+{
+    const string UpstairHallwayAddress = "24060";
+    const string MediaRoomSideLightsAddress = "58666";
+    const string GarageSwitchAddress = "28 CA 15 2";
+    const string GarageSensorAddress = "28 CA 15 1";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        HttpCookie cookie = Request.Cookies["Login"];
+        if (cookie == null || cookie.Value != "1")
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+        if (!Page.IsPostBack)
+        {
+            IsyData.ISYClient isy = new IsyData.ISYClient();
+            List<IsyData.GroupData> groups = isy.GetGroups().ToList();
+
+            // first group contains all ISY nodes
+            var garage = groups.First().Nodes.FirstOrDefault(g => g.Address == GarageSensorAddress);
+            this.garageStatus.Text = garage.Status;
+
+            var mc = groups.FirstOrDefault(g => g.Address == MediaRoomSideLightsAddress);
+            this.mediaRoomStatus.Text = mc != null ? mc.Status : "unkown status";
+
+            var upstairs = groups.FirstOrDefault(g => g.Address == UpstairHallwayAddress);
+            this.upstairsHallStatus.Text = upstairs != null ? upstairs.Status : "unkown status";            
+        }
+    }
+
+    protected void toggleGarage_Click(object sender, EventArgs e)
+    {
+        IsyData.ISYClient client = new IsyData.ISYClient();
+        client.TurnOn(GarageSwitchAddress);
+        Response.Redirect(Request.Url.ToString(), true);
+    }
+
+    protected void mediaRoomOn_Click(object sender, EventArgs e)
+    {
+        IsyData.ISYClient client = new IsyData.ISYClient();
+        client.TurnOn(MediaRoomSideLightsAddress);
+        Response.Redirect(Request.Url.ToString(), true);
+    }
+
+    protected void mediaRoomOff_Click(object sender, EventArgs e)
+    {
+        IsyData.ISYClient client = new IsyData.ISYClient();
+        client.TurnOff(MediaRoomSideLightsAddress);
+        Response.Redirect(Request.Url.ToString(), true);
+    }
+
+    protected void upstairsHallOn_Click(object sender, EventArgs e)
+    {
+        IsyData.ISYClient client = new IsyData.ISYClient();
+        client.TurnOn(UpstairHallwayAddress);
+        Response.Redirect(Request.Url.ToString(), true);
+    }
+
+    protected void upstairsHallOff_Click(object sender, EventArgs e)
+    {
+        IsyData.ISYClient client = new IsyData.ISYClient();
+        client.TurnOff(UpstairHallwayAddress);
+        Response.Redirect(Request.Url.ToString(), true);
+    }
+}
