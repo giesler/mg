9c997b72007a5b9d583d90e674343c5e32b6c042
Mike Giesler (live.com) <giesler@live.com>
2021-12-12 10:51:08 -0800

    Update to fx/sdk 4.5.2

--- Common/HomeControl/RachioIntegration.cs
+++ Common/HomeControl/RachioIntegration.cs
@@ -8,7 +8,7 @@ namespace msn2.net
 {
     class RachioIntegration
     {
-        readonly string apiToken = "63ff7170-d607-48b3-af8b-c289496d16aa";
+//        readonly string apiToken = "63ff7170-d607-48b3-af8b-c289496d16aa";
 
         // curl -v https://api.rach.io/1/public/person/info -H "Authorization: Bearer 63ff7170-d607-48b3-af8b-c289496d16aa"
         // Person ID - f444b2c3-479e-4c3d-938e-ac6fe7f86914
